3973d1375ffd4fb280165c5eb06728e9b5385ec2
Peerapat-A <6421061002@cdti.ac.th>
2023-06-14 15:31:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="style.css"/>
+
+<?php
+
+    $name = "Peerapat Akarasriprapai";
+    $age = 18;
+    $year = 2566;
+    $byear = $year-$age;
+
+    echo "<center>";
+    echo "<h1> welcome to website </h1>";
+    echo "made by $name <br/>";
+    echo "B.E. $byear <br/>";
+    echo "</center>";
+
+?>
+
+<p class="word">
+    This website is for testing coding website
+</p>
+
+<img src="images/padoru.gif" class="img1" /> 
+<img src="images/kuru-kuru.gif" class="img1"/> 
+<img src="images/aru-aru.gif" class="img1"/> 
